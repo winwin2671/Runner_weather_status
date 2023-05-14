39a8385996763c7d7d16715e16f3f0f03546197c
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-05-14 10:26:27 +0700

    don't need to hard code Lat Lon for location now

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ app = Flask(__name__)
 def index():
     city = 'Bangkok' # request.form.get("city")
     units = 'metric'  # request.form.get("units")
-    lat = 13.75  #will make it convert later
-    lon = 100.5167 #will make it convert later
+    location_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={api_key}"
+    location_response = requests.get(location_url)
+    data_loc = location_response.json()
+    lat =  data_loc[0]["lat"]
+    lon = data_loc[0]["lon"]
+    
     weather_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units={units}"
     air_url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
- 
+
     weather_response = requests.get(weather_url)
     air_response = requests.get(air_url)
 
@@ -24,6 +28,7 @@ def index():
 
     print(json.dumps(data, indent=4))
     print(json.dumps(data_air, indent=4))
+    
 
     temperature = data["main"]["temp"]
     humidity = data["main"]["humidity"]
@@ -31,19 +36,30 @@ def index():
     aqi = data_air["list"][0]["main"]["aqi"]
     #uv_index = data["list"][0]["components"]["uvi"] #openweather Deprecated it for the free version
     #precipitation = data["list"][0]["components"]["precipitation"] 
+   
+    if aqi == 1:
+        aqi_stat = 'Good'
+    elif aqi == 2:
+        aqi_stat = 'Fair'
+    elif aqi == 3:
+        aqi_stat= 'Moderate'
+    elif aqi == 4:
+        aqi_stat = 'Poor'
+    else:
+        aqi_stat = 'Very Poor'
+
 
     if temperature >= 35 and temperature <= -27:
         message = f"Temperature may make it difficult to run: {temperature}°"
-    elif humidity <= 70:
+    elif humidity <= 70: 
         message = f"Humidity may make it difficult to run as it feels like {feels_like}°"
     elif aqi >= 4:
-           message = f"Air quality may not be suitable for running. The AQI level is {aqi}"
+           message = f"Air quality may not be suitable for running. The AQI level is {aqi}, {aqi_stat}"
     else:
         message = "It's a great day for running. Enjoy!"
       
 
     print(message)
-
     return render_template("index.html", message=message)
 
 
