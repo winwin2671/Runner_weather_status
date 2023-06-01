f78c1992d500219b0efb4090fbb6930162d7ee10
winwin2671 <88149505+winwin2671@users.noreply.github.com>
2023-06-01 23:04:43 +0700

    initializing Necessary code snippets / trying to understand

--- static/mapweatherdata.js
+++ static/mapweatherdata.js
@@ -1,6 +1,26 @@
+//Initializing the Application
 tt.setProductInfo(mapweatherdata, applicationVersion)
 
 const tomTomMap = tt.map({
   key: tomTomApiKey,
   container: htmlDivId,
 })
+
+//Searching for a Location
+tt.services
+  .fuzzySearch({
+    key: tomTomApikey,
+    query: queryText,
+  })
+  .go()
+  .then(centerAndZoom)
+  .catch(function (error) {
+    alert('Could not find location (' + queryText + '). ' + error.message)
+  })
+
+function centerAndZoom(response) {
+  tomTomMap.flyTo({
+    center: response.results[0].position,
+    zoom: zoomLevel,
+  })
+}
